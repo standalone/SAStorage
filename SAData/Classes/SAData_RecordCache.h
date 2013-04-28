394a4f13ad851a2677bc1e3e0d9043e5addaf24e
Ben Gottlieb <ben@standalone.com>
2013-04-27 22:01:32 -0500

    added record cache

---
+++ SAData/Classes/SAData_RecordCache.h
@@ -0,0 +1,13 @@
+//
+//  SAData_RecordCache.h
+//  SADataTester
+//
+//  Created by Ben Gottlieb on 4/27/13.
+//  Copyright (c) 2013 Stand Alone, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SAData_RecordCache : NSObject
+
+@end
