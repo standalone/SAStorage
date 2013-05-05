230f6dac484891336362944d006b5606f4345794
Ben Gottlieb <ben@standalone.com>
2013-05-05 08:06:41 -0500

    updated sample database, improved relationship testing

--- SAStorage_Library/SAStorage_LibraryTests/SAStorage_DatabaseTests.m
+++ SAStorage_Library/SAStorage_LibraryTests/SAStorage_DatabaseTests.m
@@ -82,25 +82,51 @@
 - (void) testRecordRelationships {
 	SAStorage_Database		*db = self.emptyDB;
 	
+	
+	//set up contacts
 	NSMutableArray			*contacts = [NSMutableArray array];
-	NSArray					*fields = @[
+	NSArray					*contactFields = @[
 	   @{@"first_name": @"Barack", @"last_name": @"Obama", @"age": @(52)},
 	   @{@"first_name": @"Michelle", @"last_name": @"Obama", @"age": @(48)},
 	   @{@"first_name": @"Sasha", @"last_name": @"Obama", @"age": @(6)},
 	   @{@"first_name": @"Malia", @"last_name": @"Obama", @"age": @(8)},
 	];
 	
-	for (NSDictionary *fieldSet in fields) {
+	for (NSDictionary *fieldSet in contactFields) {
 		SAStorage_Record		*record = [db insertNewRecordOfType: @"Contact" withFields: fieldSet completion: nil];
 		STAssertNotNil(record, @"Failed to create record in database: %@");
 		[contacts addObject: record];
 	}
 	
 	contacts[0][@"spouse"] = contacts[1];
+	STAssertNotNil(contacts[1][@"spouse"], @"Failed to create back link when creating 1-1 relationship");
+	
 	contacts[0][@"kids"] = @[ contacts[2], contacts[3] ];
+	STAssertTrue([contacts[2][@"parents"] count] > 0, @"Failed to create back link when creating many-to-many relationship");
+
+	contacts[1][@"kids"] = @[ contacts[2], contacts[3] ];
 	contacts[2][@"parents"] = @[ contacts[0], contacts[1] ];
 	contacts[3][@"parents"] = @[ contacts[0], contacts[1] ];
 
+	//set up vehicles
+	NSMutableArray			*vehicles = [NSMutableArray array];
+	NSArray					*vehicleFields = @[
+			@{@"name": @"Marine One", @"make": @"Sikorsky", @"model": @"VH-60N"},
+			@{@"name": @"Air Force One", @"make": @"Boeing", @"model": @"747"},
+			@{@"name": @"Limo", @"make": @"GM", @"model": @"Stretch Limo"},
+			@{@"name": @"Bike", @"make": @"Schwinn", @"model": @"Roadmaster"},
+		];
+	
+	for (NSDictionary *fieldSet in vehicleFields) {
+		SAStorage_Record		*record = [db insertNewRecordOfType: @"Vehicle" withFields: fieldSet completion: nil];
+		STAssertNotNil(record, @"Failed to create record in database: %@");
+		[vehicles addObject: record];
+	}
+	
+	contacts[0][@"vehicles"] = [vehicles subarrayWithRange: NSMakeRange(0, 3)];
+	contacts[2][@"vehicles"] = @[ vehicles[3] ];
+
+	
 	NSError					*error = [db saveWithCompletion: nil];
 	STAssertNil(error, @"There was an error saving the database: %@", error);
 	[db deleteBackingStore];
